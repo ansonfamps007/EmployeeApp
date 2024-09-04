df48f6cf248c97c09b0030eeb744cff9b986e9ff
Anson Francis <57131347+ansonfamps007@users.noreply.github.com>
2024-09-04 20:46:19 +0200

    Added docker-compose and kubernete files

--- src/main/java/com/app/employee/exception/ResourceNotFoundException.java
+++ src/main/java/com/app/employee/exception/ResourceNotFoundException.java
@@ -2,7 +2,9 @@ package com.app.employee.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+//@RestControllerAdvice
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
